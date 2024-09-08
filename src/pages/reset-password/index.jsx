cd46e7216860aad35a6dd0ed4a590201b9d56af9
frst1009 <fttkpsttkp@gmail.com>
2024-09-08 14:20:45 +0400

    changes

--- src/pages/reset-password/index.jsx
+++ src/pages/reset-password/index.jsx
@@ -1,15 +1,15 @@
-import React from 'react'
+import React from "react";
 
-import Navbar from "../../components/SigninNavbar"
-import Reset from '../../components/ResetPassword/Reset'
+import Navbar from "../../components/SigninNavbar/index";
+import Reset from "../../components/ResetPassword/Reset";
 
 const index = () => {
-    return (
-        <div className='container'>
-            <Navbar />
-            <Reset />
-        </div>
-    )
-}
+  return (
+    <div className="container">
+      <Navbar />
+      <Reset />
+    </div>
+  );
+};
 
-export default index
+export default index;
